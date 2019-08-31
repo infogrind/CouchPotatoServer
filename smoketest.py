cc04100e1d7486f8acd5657c18075b3b5ea23686
Marius Kleiner <kleiner@gmail.com>
2019-08-31 10:47:06 +0200

    Using 'with' syntax for test set-up/teardown
    This allows to move the set-up and teardown code into separate methods,
    without requiring global state for things like the temporary directory
    name. It makes writing the test much cleaner.

--- smoketest.py
+++ smoketest.py
@@ -21,35 +21,52 @@ import tempfile
 import time
 
 
+class TestContext:
+    def __enter__(self):
+        self.tmpdirname = tempfile.mkdtemp()
+        print "Temporary directory is {}".format(self.tmpdirname)
+        shutil.copyfile(TEST_SETTINGS, self.tmpdirname + "/settings.conf")
+
+        # Use temporary directory as data directory; it will be deleted again
+        # below.
+        args = DEFAULT_ARGS + "--data_dir {}".format(self.tmpdirname).split()
+        print "Running application in subprocess"
+        self.child = subprocess.Popen([PROGRAM] + args)
+        print "Subrocess is running. Waiting {} seconds to warm up.".format(
+                WARMUP_TIME_SEC)
+        time.sleep(WARMUP_TIME_SEC)
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Note: we don't check if there were any exceptions; we want to
+        # terminate in any case.
+        try:
+            self.child.terminate()
+
+            # TODO(marius): Find out how to use a timeout here and force the
+            # process to terminate in case it doesn't react.
+            print ("Sent SIGTERM, waiting for child process {} to "
+            "terminate.").format(self.child.pid)
+            self.child.wait()
+            self.child = None
+
+        finally:
+            # Clean up temporary directory
+            try:
+                shutil.rmtree(self.tmpdirname)
+            except:
+                print "Error removing temporary directory {}".format(
+                        self.tmpdirname)
+
+
+
+
 def main():
     """
     Main function
     """
-    try:
-        tmpdirname = tempfile.mkdtemp()
-        print "Temporary directory is {}".format(tmpdirname)
-        shutil.copyfile(TEST_SETTINGS, tmpdirname + "/settings.conf")
-
-        # Use temporary directory as data directory; it will be deleted again
-        # below.
-        args = DEFAULT_ARGS + "--data_dir {}".format(tmpdirname).split()
-        print "Running application in subprocess"
-        child = subprocess.Popen([PROGRAM] + args)
-        print "Subrocess is running. Waiting {} seconds to warm up.".format(
-                WARMUP_TIME_SEC)
-        time.sleep(WARMUP_TIME_SEC)
-        child.terminate()
-
-        # TODO(marius): Should use a timeout
-        print "Sent SIGTERM, waiting for child process {} to terminate.".format(
-                child.pid)
-        child.wait()
-
-    finally:
-        try:
-            shutil.rmtree(tmpdirname)
-        except:
-            print "Error removing temporary directory {}".format(tmpdirname)
+    with TestContext():
+        pass
 
 
 if __name__ == "__main__":
